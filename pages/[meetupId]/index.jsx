41a6b85a7359f0cdcceb393018b395d63bcfd75f
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-28 20:10:32 -0500

    Adding a new component and new styles to display dummy data in the dynamic page.

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,3 +1,21 @@
-export default function MeetupDetailPage(){
-    return <h1>Meetup Page</h1>
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const M1 = {
+  id: "m1",
+  title: "First Meetup",
+  image:
+    "https://www.las2orillas.co/wp-content/uploads/2024/09/San-francisco-3.jpg",
+  address: "San Francisco's Park",
+  description: "This is a first meetup!",
+};
+
+export default function MeetupDetailPage() {
+  return (
+    <MeetupDetail
+      title={M1.title}
+      image={M1.image}
+      address={M1.address}
+      description={M1.description}
+    />
+  );
 }
