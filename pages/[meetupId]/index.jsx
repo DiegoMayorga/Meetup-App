5eca20b0bf0960206cd78fb34465bc6db06aab70
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-01 08:53:46 -0500

    Using the getStaticProps function in the dynamic route.

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,21 +1,42 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const M1 = {
+/* const M1 = {
   id: "m1",
   title: "First Meetup",
   image:
     "https://www.las2orillas.co/wp-content/uploads/2024/09/San-francisco-3.jpg",
   address: "San Francisco's Park",
   description: "This is a first meetup!",
-};
+}; */
 
-export default function MeetupDetailPage() {
+export default function MeetupDetailPage(props) {
   return (
     <MeetupDetail
-      title={M1.title}
-      image={M1.image}
-      address={M1.address}
-      description={M1.description}
+      title={props.meetupData.title}
+      image={props.meetupData.image}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticProps(context) {
+  // fetch data
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: "m1",
+        title: "First Meetup",
+        image:
+          "https://www.las2orillas.co/wp-content/uploads/2024/09/San-francisco-3.jpg",
+        address: "San Francisco's Park",
+        description: "This is a first meetup!",
+      },
+    },
+  };
 }
